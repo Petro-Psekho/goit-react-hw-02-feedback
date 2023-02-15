f9510aa81aa276dbd080fc02a032d910fe11348e
Petro Psekho <psekho.pp@gmail.com>
2023-02-15 21:52:43 +0200

    add metodth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,36 @@ class App extends Component {
     bad: 0,
   };
 
-  handleIncrement = e => {
-    this.setState(prevState => {
-      return {
-        good: prevState + 1,
-      };
-    });
+  goodIncrement = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  neutralIncrement = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  badIncrement = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    let positiveFeedback = 0;
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    positiveFeedback = (good / total) * 100;
+    return positiveFeedback;
   };
 
   render() {
@@ -24,19 +48,21 @@ class App extends Component {
       <Container>
         <div>
           <h3>Please leave feedback</h3>
-          <button type="button" onClick={this.handleIncrement}>
+          <button type="button" onClick={this.goodIncrement}>
             Good
           </button>
-          <button type="button" onClick={this.handleIncrement}>
+          <button type="button" onClick={this.neutralIncrement}>
             Neutral
           </button>
-          <button type="button" onClick={this.handleIncrement}>
+          <button type="button" onClick={this.badIncrement}>
             Bad
           </button>
           <h3>Statistics</h3>
           <p>Good: {good} </p>
           <p>Neutral: {neutral}</p>
           <p>Bad: {bad}</p>
+          <p>Total: {this.countTotalFeedback()}</p>
+          <p>Positive feedback: {this.countPositiveFeedbackPercentage()}%</p>
         </div>
       </Container>
     );
